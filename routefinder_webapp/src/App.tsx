bedbfd1f6bbd93fe29b8c66f9986bb76d0aeafee
Kosta Kai Ugrina <kkugrina@gmail.com>
2024-11-09 15:14:04 +0100

    test auto-deploy
    my girlfriend wanted to see it

--- routefinder_webapp/src/App.tsx
+++ routefinder_webapp/src/App.tsx
@@ -11,6 +11,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <p>Test for auto-deploy</p>
+        <p>Bok Iro</p>
         <a
           className="App-link"
           href="https://reactjs.org"
