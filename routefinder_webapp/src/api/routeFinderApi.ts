0a16f285dc8cccf0e4231b2342c647ba6c5e803f
Kosta Kai Ugrina <kkugrina@gmail.com>
2024-11-11 16:44:47 +0100

    switch to non-localhost API URL

--- routefinder_webapp/src/api/routeFinderApi.ts
+++ routefinder_webapp/src/api/routeFinderApi.ts
@@ -1,9 +1,10 @@
 import { Address } from "../context/AddressContext";
 import axios from "axios";
 
-// const API_URL = "routefinderapi-gacyatghdxb0dafz.westeurope-01.azurewebsites.net";
+const API_URL =
+  "routefinderapi-gacyatghdxb0dafz.westeurope-01.azurewebsites.net";
 
-const API_URL = "http://localhost:5295";
+// const API_URL = "http://localhost:5295";
 
 export async function testConnection(): Promise<string> {
   try {
