b0c5c0373c8ef45ac5f100ae782db7ed641cb3f3
Kosta Kai Ugrina <kkugrina@gmail.com>
2024-11-11 16:40:42 +0100

    add wip basic request

---
+++ routefinder_webapp/src/api/routeFinderApi.ts
@@ -0,0 +1,32 @@
+import { Address } from "../context/AddressContext";
+import axios from "axios";
+
+// const API_URL = "routefinderapi-gacyatghdxb0dafz.westeurope-01.azurewebsites.net";
+
+const API_URL = "http://localhost:5295";
+
+export async function testConnection(): Promise<string> {
+  try {
+    const response = await axios.get(`${API_URL}`);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching user:", error);
+    throw error;
+  }
+}
+
+export async function findFastestRoute(
+  addressStart: Address,
+  addressDestinationList: Address[]
+): Promise<Address[]> {
+  try {
+    const response = await axios.post(`${API_URL}/fastest-route`, {
+      addressStart,
+      addressDestinationList,
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching user:", error);
+    throw error;
+  }
+}
--- routefinder_webapp/src/components/AddressInputForm/AddressInputForm.tsx
+++ routefinder_webapp/src/components/AddressInputForm/AddressInputForm.tsx
@@ -1,6 +1,7 @@
 import { Autocomplete, Button, Stack, TextField } from "@mui/material";
 import React, { FC } from "react";
-import { useAddressContext } from "../../context/AddressContext";
+import { Address, useAddressContext } from "../../context/AddressContext";
+import { findFastestRoute, testConnection } from "../../api/routeFinderApi";
 
 type Props = {};
 
@@ -39,7 +40,56 @@ export const AddressInputForm: FC<Props> = (props: Props) => {
         options={options.map((option) => option)}
         renderInput={(params) => <TextField {...params} label="Point B" />}
       />
-      <Button onClick={() => console.log("oh hai")}>Find route</Button>
+      <Stack direction="row" spacing={2}>
+        <Button variant="contained">Find route</Button>
+        <Button
+          variant="contained"
+          onClick={() => {
+            testConnection().then((response) => {
+              console.log(response);
+            });
+          }}
+        >
+          Test Connection
+        </Button>
+        <Button
+          variant="contained"
+          onClick={() => {
+            const netto: Address = {
+              name: "Netto on Artillerivej",
+              latitude: 55.66020591154689,
+              longitude: 12.572785483070883,
+            };
+            findFastestRoute(addressStart, [netto]).then((response) => {
+              console.log(response);
+            });
+          }}
+        >
+          Test 1
+        </Button>
+        <Button
+          variant="contained"
+          onClick={() => {
+            const cafeNoah: Address = {
+              name: "Cafe Noah",
+              latitude: 55.65509242630194,
+              longitude: 12.565147723042628,
+            };
+            const netto: Address = {
+              name: "Netto on Artillerivej",
+              latitude: 55.66020591154689,
+              longitude: 12.572785483070883,
+            };
+            findFastestRoute(addressStart, [cafeNoah, netto]).then(
+              (response) => {
+                console.log(response);
+              }
+            );
+          }}
+        >
+          Test 2
+        </Button>
+      </Stack>
     </Stack>
   );
 };
