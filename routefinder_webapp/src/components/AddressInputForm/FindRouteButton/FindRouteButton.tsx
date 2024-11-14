0907c2c081363b5214ba118dee6c27d92ec99e8b
Kosta Kai Ugrina <kkugrina@gmail.com>
2024-11-14 17:34:48 +0100

    add rudimentary invalid request data checking

--- routefinder_webapp/src/components/AddressInputForm/FindRouteButton/FindRouteButton.tsx
+++ routefinder_webapp/src/components/AddressInputForm/FindRouteButton/FindRouteButton.tsx
@@ -2,23 +2,33 @@ import { Button } from "@mui/material";
 import React, { FC } from "react";
 import { useAddressContext } from "../../../context/AddressContext";
 import { findFastestRoute } from "../../../api/routeFinderApi";
+import { Address } from "../../../types";
 
 export const FindRouteButton: FC = () => {
   const { addressStart, addressDestinationList } = useAddressContext();
+  const makeRequest = () => {
+    const addressDestinationListFixed = fixDestinationList(
+      addressDestinationList
+    );
+    const result = findFastestRoute({
+      addressStart,
+      addressDestinationList: addressDestinationListFixed,
+    });
+    console.log("FIND ROUTE RESULT");
+    console.log(result);
+  };
+
   return (
-    <Button
-      color="primary"
-      variant="contained"
-      onClick={() => {
-        const result = findFastestRoute({
-          addressStart,
-          addressDestinationList,
-        });
-        console.log("FIND ROUTE RESULT");
-        console.log(result);
-      }}
-    >
+    <Button color="primary" variant="contained" onClick={makeRequest}>
       Find Route
     </Button>
   );
 };
+
+function fixDestinationList(addressDestinationList: Address[]): Address[] {
+  const fixed = addressDestinationList.filter(
+    (addr) => addr.latitude !== undefined && addr.longitude !== undefined
+  );
+
+  return fixed;
+}
